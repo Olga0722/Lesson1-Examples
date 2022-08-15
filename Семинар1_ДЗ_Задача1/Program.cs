14ff2c65224e92e1f1f60494a90b5000ebbb3bc6
olga <o.v.matvienko@yandex.ru>
2022-08-15 14:53:55 +0300

    add Добавлен поиск минимума

--- Семинар1_ДЗ_Задача1/Program.cs
+++ Семинар1_ДЗ_Задача1/Program.cs
@@ -4,15 +4,22 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 
-!!! ВОПРОС: Я НЕ ЗНАЮ, КАКОЙ КОМАНДОЙ/ОПЕРАЦИЕЙ ПОКАЗАТЬ, ЧТО У ЧИСЛА А ЕСТЬ ТРИ ВАРИАНТА (5, 2, -9)
+!!! ВОПРОС: Я НЕ ЗНАЮ, КАКОЙ КОМАНДОЙ/ОПЕРАЦИЕЙ/строкой ПОКАЗАТЬ, ЧТО У ЧИСЛА А ЕСТЬ ТРИ ВАРИАНТА (5, 2, -9)
 И У ЧИСЛА Б ТОЖЕ ТРИ ВАРИАНТА (7, 10, -3)*/
 
 int a = 5;
 int b = 7;
 
 int max = a;
+int min = b;
 if (a > max ) max = a;
 if (b > max ) max = b;
+if (a < min ) min = a;
+if (b < min ) min = b;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
