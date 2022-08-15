cfcd2a2592d21c7dc78a946a6ae693f35d5d74ee
olga <o.v.matvienko@yandex.ru>
2022-08-15 23:09:01 +0300

    add Семинар 1 ДЗ Задача 8

---
+++ Семинар1_ДЗ_Задача8/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число 1: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите число 2: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(n + " ");
+            }
+        }
+        Console.ReadKey();
