3fc5420a462c71e365619b06ba47c9f98494787e
olga <o.v.matvienko@yandex.ru>
2022-08-15 22:56:28 +0300

    add Семинар 1 ДЗ Задача 6

---
+++ Семинар1_ДЗ_Задача6/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число : ");
+                int a = Convert.ToInt32(Console.Read());
+                if (a % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+                Console.ReadKey();
