e3d80388fc43002384ed414163fd8c6a3c6d74fc
olga <o.v.matvienko@yandex.ru>
2022-08-29 03:19:22 +0300

    add Задача 13

---
+++ Задача_13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+if (num.Length == 3)
+{Console.WriteLine("Третья цифра числа: " + num[2]);}
+if (num.Length < 3)
+{Console.WriteLine("Третьей цифры нет");}
+if (num.Length > 3)
+{Console.WriteLine("Третья цифра числа: " + num[2]);}
