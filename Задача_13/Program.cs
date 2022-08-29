d6b26df287fd8fd23734beaafa4d2a06833a76f2
olga <o.v.matvienko@yandex.ru>
2022-08-29 03:35:00 +0300

    add Объединение условий в одну строку

--- Задача_13/Program.cs
+++ Задача_13/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.WriteLine("Введите число");
 string num = Console.ReadLine();
-if (num.Length == 3)
+if (num.Length == 3 || num.Length > 3)
 {Console.WriteLine("Третья цифра числа: " + num[2]);}
 if (num.Length < 3)
 {Console.WriteLine("Третьей цифры нет");}
-if (num.Length > 3)
-{Console.WriteLine("Третья цифра числа: " + num[2]);}
+
