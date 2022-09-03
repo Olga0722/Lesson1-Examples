014c0ea0c31a885bcdd6d59216c519916ecb3877
olga <o.v.matvienko@yandex.ru>
2022-09-04 01:21:45 +0300

    add Задача 15

---
+++ Задача_15/Program.cs
@@ -0,0 +1,14 @@
+﻿{
+    Dictionary<int, string>eve = new Dictionary<int, string>()
+    {
+       {1, "ПН - рабочий день"},
+       {2, "ВТ - рабочий день"},
+       {3, "ср - рабочий день"},
+       {4, "чтв - рабочий день"},
+       {5, "Пт - рабочий день"},
+       {6, "суб - рабочий день"},
+       {7, "вск - рабочий день"},
+    };
+    int index = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(eve[index]);
+}
